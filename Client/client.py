9477958dbb33b8e821c29123f8e473cac420347c
PriyanshuKSharma <priyanshu17ks@gmail.com>
2024-04-28 14:38:53 +0530

    OTP will be displayed on GUI

--- Client/client.py
+++ Client/client.py
@@ -1,29 +1,33 @@
 import tkinter as tk
 import socket
-import hashlib
-
-def generate_hash(password):
-    return hashlib.sha256(password.encode()).hexdigest()
 
 def authenticate():
     username = username_entry.get()
     password = password_entry.get()
-    password_hash = generate_hash(password)  # Generate hash for the password
-    
+    otp = otp_entry.get()  # Get the entered one-time password
+
     host = '127.0.0.1'
     port = 12345
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    data = username + ',' + password_hash  # Send username and password hash
-    client_socket.send(data.encode())
+    # Send username, password, and OTP to server
+    client_socket.send(username.encode())
+    client_socket.send(password.encode())
+    client_socket.send(otp.encode())  # Send OTP to the server
 
-    response = client_socket.recv(1024).decode()
-    result_label.config(text=response)
+    # Receive login result from server
+    login_result = client_socket.recv(1024).decode()
 
+    # Close client socket
     client_socket.close()
 
+    # Update GUI based on login result
+    if login_result.startswith("Login successful"):
+        result_label.config(text=login_result)
+    else:
+        result_label.config(text=login_result)
 
 # GUI setup
 root = tk.Tk()
@@ -39,10 +43,16 @@ password_label.grid(row=1, column=0, padx=10, pady=5)
 password_entry = tk.Entry(root, show="*")
 password_entry.grid(row=1, column=1, padx=10, pady=5)
 
+# Hide the OTP entry widget
+otp_label = tk.Label(root, text="One-time Password:")
+otp_label.grid(row=2, column=0, padx=10, pady=5)
+otp_entry = tk.Entry(root, show="*")  # Show the entry widget but with a password mask
+otp_entry.grid(row=2, column=1, padx=10, pady=5)
+
 login_button = tk.Button(root, text="Login", command=authenticate)
-login_button.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+login_button.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
 
 result_label = tk.Label(root, text="")
-result_label.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+result_label.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
 
 root.mainloop()
